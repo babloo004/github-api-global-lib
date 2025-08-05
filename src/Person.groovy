353d817b52f0bd9008759e60a20d41724ac2fc49
Avinash <129788500+babloo004@users.noreply.github.com>
2025-08-06 03:15:51 +0530

    Update Person.groovy

--- src/Person.groovy
+++ src/Person.groovy
@@ -1,19 +1,22 @@
 
 
 class Person {
+  def steps;
   String name;
   int age;
 
   //constructor
-  Person() {
+  Person(def steps) {
     this.name=null;
     this.age=0;
+    this.steps=steps;
   }
 
   //custom constructor
-  Person(String name,int age) {
+  Person(String name,int age,def steps) {
     this.name=name;
     this.age=age;
+    this.steps=steps;
   }
 
   //methods
