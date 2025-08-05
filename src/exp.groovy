fb0261f142d05f1bbdfeaf24829da31c7bc66311
Avinash <129788500+babloo004@users.noreply.github.com>
2025-08-06 03:00:11 +0530

    Update exp.groovy

--- src/exp.groovy
+++ src/exp.groovy
@@ -1,3 +1,22 @@
-def f(String name="human"){
-  echo "Hi ${name}!"
+class Person {
+  String name;
+  int age;
+
+  //constructor
+  Person() {
+    this.name=null;
+    this.age=0;
+  }
+
+  //custom constructor
+  Person(String name,int age) {
+    this.name=name;
+    this.age=age;
+  }
+
+  //methods
+  void details(){
+    println("Name : ${name}!");
+    println("age : ${age}!");
+  }
 }
